1ad42e7ba6f232b262dd651e98beb3477690dac8
Peter Calabrese <peter@thirtyfour.tech>
2020-12-05 19:32:12 -0500

    Added logic to handle spinner

--- app/src/main/java/com/example/scoop2u/registration.java
+++ app/src/main/java/com/example/scoop2u/registration.java
@@ -3,12 +3,19 @@ package com.example.scoop2u;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class registration extends AppCompatActivity {
+    private Spinner accounttype;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        accounttype = (Spinner) findViewById(R.id.accountType);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.account_type, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        accounttype.setAdapter(adapter);
     }
 }
