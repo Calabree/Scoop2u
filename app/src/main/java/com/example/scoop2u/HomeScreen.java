f4cdef64469ff316dde460b08ea97999eb485694
Nicholas Crayne <nickcrayne@gmail.com>
2020-12-08 22:47:46 -0500

    Setup for the Home Screen, to be used to change between fragments.

---
+++ app/src/main/java/com/example/scoop2u/HomeScreen.java
@@ -0,0 +1,17 @@
+package com.example.scoop2u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+}
