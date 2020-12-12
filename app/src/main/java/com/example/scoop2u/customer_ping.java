b494745778f71b56cbf65702a40bcb2d4adfbbc3
Peter Calabrese <peter@thirtyfour.tech>
2020-12-11 23:25:17 -0500

    added address handle in registration

---
+++ app/src/main/java/com/example/scoop2u/customer_ping.java
@@ -0,0 +1,53 @@
+package com.example.scoop2u;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class customer_ping extends Fragment {
+    private Button ping, address;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    private DatabaseReference reference;
+    private FirebaseUser user;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_gmaps, container, false);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String latitude = snapshot.child("Users").child(user.getUid()).child("latitude").getValue().toString();
+                String longitude = snapshot.child("Users").child(user.getUid()).child("latitude").getValue().toString();
+                //accountType.setText(type);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        return view;
+    }
+}
