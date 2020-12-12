8382d7e5ade9c17da11eeb2294e95ffb16be8748
Jake Gusew <guseja@farmingdale.edu>
2020-12-12 13:27:42 -0500

    Customer Map

--- app/src/main/java/com/example/scoop2u/gmapsFragmentCustomer.java
+++ app/src/main/java/com/example/scoop2u/gmapsFragmentCustomer.java
@@ -42,8 +42,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import static android.os.SystemClock.sleep;
-
 public class gmapsFragmentCustomer extends Fragment implements OnMapReadyCallback, View.OnClickListener {
 
     private MapView map;
@@ -305,11 +303,11 @@ public class gmapsFragmentCustomer extends Fragment implements OnMapReadyCallbac
 
 
         FirebaseDatabase.getInstance().getReference().child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
-
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        orderInProgress = false;
+
                         double distance = 0;
+                        boolean orderInProgress = true;
                         for (DataSnapshot snap : snapshot.getChildren()) {
 
                             String type = snap.child("accountType").getValue().toString();
@@ -326,19 +324,16 @@ public class gmapsFragmentCustomer extends Fragment implements OnMapReadyCallbac
                                     System.out.println(d + " Miles");
 
                                     distance = d;
-                                    String driverEmail = snap.getKey();
-                                    //String driverEmail = snap.child("email").getValue().toString();
+
+                                    String driverEmail = snap.child("email").getValue().toString();
 
                                     FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("currentDriverID").setValue(driverEmail);
-
-
-
                                     orderInProgress = true;
                                     System.out.println("Closest Driver: " + driverEmail);
                                 }
                             }
                         }
-
+                        activeOrder(orderInProgress);
                     }
 
                     @Override
@@ -349,7 +344,8 @@ public class gmapsFragmentCustomer extends Fragment implements OnMapReadyCallbac
 
                 });
 
-                activeOrder(orderInProgress);
+
+
     }
 
     private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
@@ -362,7 +358,6 @@ public class gmapsFragmentCustomer extends Fragment implements OnMapReadyCallbac
     }
 
     private void activeOrder(boolean orderInProgress) {
-
         while (orderInProgress) {
 
             FirebaseDatabase.getInstance().getReference().child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
@@ -370,10 +365,12 @@ public class gmapsFragmentCustomer extends Fragment implements OnMapReadyCallbac
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                    String currentDriver = snapshot.child("currentDriverID").getValue().toString();
-                    double lat2 = Double.parseDouble(snapshot.child(currentDriver).child("latitude").getValue().toString());
-                    double lon2 = Double.parseDouble(snapshot.child(currentDriver).child("longitude").getValue().toString());
-                    System.out.println("driver lat:"+lat2+",drver lon:"+lon2);
+                    String driveID = snapshot.child("currentDriverID").getValue().toString();
+
+                    String test = FirebaseDatabase.getInstance().getReference("Users").child(driveID).getParent().toString();
+
+                    System.out.println(test);
+                    //FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("longitude").setValue(location.getLongitude());
 
 
                 }
@@ -383,9 +380,6 @@ public class gmapsFragmentCustomer extends Fragment implements OnMapReadyCallbac
 
                 }
             });
-
-            sleep(5000);
-            System.out.println("Hey, We slept for 5 seconds!");
         }
     }
 }
